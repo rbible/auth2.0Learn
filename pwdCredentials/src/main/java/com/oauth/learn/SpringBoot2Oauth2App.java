b7fe0f5df74a0e270dfbd1728dc2065ec7699dfd
leixiangjian@bolemy.com <leixiangjian@bolemy.com>
2018-09-14 19:28:38 +0800

    密码模式与客户端模式

---
+++ pwdCredentials/src/main/java/com/oauth/learn/SpringBoot2Oauth2App.java
@@ -0,0 +1,13 @@
+package com.oauth.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2Oauth2App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot2Oauth2App.class, args);
+    }
+
+}
